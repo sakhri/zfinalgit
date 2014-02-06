878893f2300264fd19ab76bba308175a88703b1c
sakhri <medaminesakhri@gmail.com>
2014-02-06 02:00:09 +0100

    first commit

---
+++ finaltestgit/src/finaltestgit/final1.java
@@ -0,0 +1,24 @@
+package finaltestgit;
+
+public class final1 {
+
+
+	
+public void afficher()
+{
+	System.out.println("premier test deuxiéme fichier");
+}
+
+public void afficher2()
+{
+	System.out.println("deuxiéme test deuxiéme fichier");
+}
+public int calculer()
+{
+	int a=3,b=2,c=0;
+	c=a+b;
+	return c;
+}
+
+
+}
